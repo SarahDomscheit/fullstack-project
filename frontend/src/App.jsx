f438d55aef370dee864467e0947e8f3c7f71c377
SarahDomscheit <sarri91@googlemail.com>
2025-05-20 13:46:49 +0200

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
-
+import { Routes, Route } from "react-router";
 import "./App.css";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./components/Dashboard";
+import { useState } from "react";
 
 function App() {
-  return <></>;
+  const [currentUser, setCurrentUser] = useState(null);
+  return (
+    <>
+      <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute currentUser={currentUser}>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
